9e02ac898bc8388b83ed8507a7aa53acc57d33e7
Konrad K Sobon <sobon.konrad@gmail.com>
2018-10-27 18:55:05 -0400

    The version it is working

--- thesaurus/thesaurus/TrainView.xaml.cs
+++ thesaurus/thesaurus/TrainView.xaml.cs
@@ -12,11 +12,7 @@
 
         private void Train_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            // TODO: Parse all the files here
-            foreach (var f in (this.DataContext as TrainViewModel).Files)
-            {
-                
-            }   
+            ParseDYN.ParseDynData((this.DataContext as TrainViewModel).Files); 
         }
     }
 }

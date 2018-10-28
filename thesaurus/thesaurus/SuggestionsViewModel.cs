a5918e85ef6294d8b44daa96a4a950c429c2a7f3
Varvara Toulkeridou <varvara.toulkeridou@autodesk.com>
2018-10-28 09:54:35 -0400

    Try Catch to handle nodes not found in the dictionary

--- thesaurus/thesaurus/SuggestionsViewModel.cs
+++ thesaurus/thesaurus/SuggestionsViewModel.cs
@@ -22,8 +22,6 @@ namespace thesaurus
             this.Model.DynamoViewModel.Model.CurrentWorkspace.NodeAdded += delegate(NodeModel nodeModel)
             {
                 string[] predictions = model.Predict(nodeModel.CreationName);
-                // TODO: Hook up with running ML module here and provide nodeModel.CreationName as input
-                // Then construct a SuggestionsNodeViewModel based on that info, the panel should update automatically
                 foreach (var predictedNode in predictions)
                 {
                     Nodes.Add(new SuggestionsNodeViewModel(model) { NodeName = predictedNode.Split('@')[0] });

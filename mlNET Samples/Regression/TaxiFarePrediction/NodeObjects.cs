fbffbfb60d853cb0c4c8533185ab95a0f12659fe
Petr Mitev <p.mitevpi@gmail.com>
2018-10-28 10:17:51 -0400

    added sample for debug

---
+++ mlNET Samples/Regression/TaxiFarePrediction/NodeObjects.cs
@@ -0,0 +1,33 @@
+﻿// <Snippet1>
+using Microsoft.ML.Runtime.Api;
+// </Snippet1>
+
+namespace TaxiFarePrediction
+{
+    // <Snippet2>
+    public class NodeObject
+    {
+        [Column("0")]
+        public string NodeAName;
+
+        [Column("2")]
+        public float CountAllConnections;
+        
+        [Column("3")]
+        public float CountUniqueConnections;
+        
+        [Column("4")]
+        public string NodeType;
+
+        [Column("1")]
+        public string NodeBName;
+
+    }
+
+    public class NodePrediction
+    {
+        [ColumnName("Score")]
+        public string NodeBName;
+    }
+    // </Snippet2>
+}
